0c0821ca4c6505d49412dc0152ccf441cf271672
MaJeD BoJaN <bojanmajed@gmail.com>
2024-08-04 16:11:00 +0300

    working on the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 
 import Partners from '@/components/partners';
+import Button from "@/components/button/button";
 
 export default function Home() {
   return (
@@ -13,8 +14,28 @@ export default function Home() {
         </div>
       </div>
 
-      <div>
+      <div className="mb-[230px]">
         <Partners />
+      </div>
+      <div className="flex flex-col items-center space-y-2 mb-[232px]">
+        <div className="mb-[55px]">
+          <div className="flex items-end">
+            <div>
+              <p className="text-white font-semibold text-4xl leading-[54px]">من نحن؟</p>
+            </div>
+            <div  className="w-[108px] h-[9px] bg-blueShade rounded-[3px]" style={{ top: '1392px', left: '725px' }}></div>
+          </div>
+          <p className="text-white font-semibold text-6xl leading-[80px]">تعرف علينا</p>
+        </div>
+         <div className="text-center">
+          <p className="text-2xl text-white font-medium	">”ليبات“ مبادرة شبابية تقنية تأسست مطلع عام 2024م.</p>
+          <p className="text-2xl text-white font-medium	">نسعى لإحداث نقلات إلى المستقبل من خلال تحقيق تحول رقمي شامل في مجالات الاقتصاد، </p>
+          <p className="text-2xl text-white font-medium	">الصحة، التعليم، الثقافة، والفن.</p>
+         </div>
+
+         <div>
+           <Button label="تعرف علينا أكثر"/>
+         </div>
       </div>
     </main>
   );
--- src/components/Button/button.jsx
+++ src/components/Button/button.jsx
@@ -1,7 +1,7 @@
 "use client"
 import styles from './button.module.css'
-export default function Button() {
+export default function Button({label}) {
   return (
-    <button className={styles.logout} onClick={()=>{console.log("logout")}}>logout</button>
-)
+    <button className={styles.logout} onClick={() => { console.log("logout") }}>{label}</button>
+  )
 }
