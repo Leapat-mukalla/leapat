166fe047434168dc5cc5d3f90b49ffe44dd9b047
Zainab Mohamed <129354366+ZainbMohammed@users.noreply.github.com>
2024-08-25 15:38:44 +0300

    Update tailwind.config.ts
    Co-authored-by: Muhammad Al-jefry <algfry12@gmail.com>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -55,24 +55,8 @@ const config = {
           foreground: "hsl(var(--card-foreground))",
         },
       },
-      // backgroundImage: {
-      //   'custom-gradient': 'linear-gradient(to right, #986CAC, #9A9A9A, #75A3B1, #262626, #FFFFFF)',
-      // },
-      // backgroundImage: {
-      //   'custom-gradient': 'linear-gradient(to right, #986CAC 0%, #986CAC 20%, #9A9A9A 20%, #9A9A9A 40%, #75A3B1 40%, #75A3B1 60%, #262626 60%, #262626 80%, #FFFFFF 80%, #FFFFFF 100%)',
-      //   back: "url('./svgs/bg.png')"
-      // },
-      // backgroundImage: {
-      //   'custom-gradient': 'linear-gradient(to top, #986CAC 100%, #9A9A9A 60%, #75A3B1 30%, #262626 10%, #FFFFFF 0%)',
-      // },
       backgroundImage: {
-
-        // 'custom-gradient': 'linear-gradient(to top right , rgba(0, 0, 0, 0.7), rgba(117, 163, 177, 0.7) 25%, rgba(154, 154, 154, 0.7) 50%, rgba(152, 108, 172, 0.7) 75%, rgba(0, 0, 0, 0.7))', // good
-        // 'symbols-background' :"url('/src/svgs/Symbols.svg')",
-
         'custom-gradient': "linear-gradient(to right, rgba(152,108,172, 0.5), rgba(154,154,154, 0.5), rgba(117,163,177, 0.5)), url('/src/svgs/Symbols.svg')",
-
-
       },
       borderRadius: {
         lg: "var(--radius)",
