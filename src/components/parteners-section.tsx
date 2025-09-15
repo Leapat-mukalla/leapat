c44935f484b21b36ec351c7845019515a4f298da
Ali abdullah Bamohammad <adeyny2005@gmail.com>
2025-09-15 08:10:11 +0300

    Improves partners section display
    Enhances the partners section by adding a conditional check for partner logos.
    
    It also adds a message indicating that no partner logos are available if the partners array is empty.
    Additionally, it includes sizes attribute to the Image component for better responsiveness.

--- src/components/parteners-section.tsx
+++ src/components/parteners-section.tsx
@@ -7,6 +7,7 @@ export default async function PartenersSection() {
   const hassenProject = await getProject("hassen");
   const partners = hassenProject.data.partners || [];
 
+
   return (
     <section className="container mx-auto my-20 max-sm:mx-4">
       <div className="mb-12 text-center">
@@ -25,16 +26,23 @@ export default async function PartenersSection() {
         </p>
       </div>
       <div className="mt-6 flex flex-wrap justify-center gap-10">
-        {partners.map((logo: string, idx: number) => (
-          <div className="h-18 relative w-44" key={idx}>
-            <Image
-              fill
-              src={logo}
-              alt={`شريك ${idx + 1}`}
-              className="object-contain"
-            />
+        {partners.length > 0 ? (
+          partners.map((logo: string, idx: number) => (
+            <div className="relative h-20 w-44" key={idx}>
+              <Image
+                fill
+                src={logo}
+                alt={`شريك ${idx + 1}`}
+                className="object-contain"
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              />
+            </div>
+          ))
+        ) : (
+          <div className="text-center text-gray-500">
+            <p>لا توجد صور شركاء متاحة</p>
           </div>
-        ))}
+        )}
       </div>
     </section>
   );
