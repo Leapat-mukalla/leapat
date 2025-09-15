516aeaeeca85b9c1ac5d52bf64dc825f00013d16
Ali abdullah Bamohammad <adeyny2005@gmail.com>
2025-09-15 07:44:37 +0300

    Adds partners section to relevant blog posts
    Adds a "Partners Section" to blog posts related to the "Hassen" project.
    
    The section includes logos and information about the organizations supporting the awareness campaign.
    It dynamically appears only on blog posts tagged or categorized as related to "Hassen" or "حصين".
    
    A new component, `PartenersSection`, is introduced to render the section.

---
+++ src/components/parteners-section.tsx
@@ -0,0 +1,43 @@
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+
+export default function PartenersSection() {
+  return (
+    <section className="container mx-auto my-20 max-sm:mx-4">
+      <div className="mb-12 text-center">
+        <h2
+          className={cn(
+            "mt-18 mb-4 flex items-end justify-center gap-2 text-3xl text-primary lg:text-5xl",
+          )}
+        >
+          الشركاء
+          <span className="mb-2 inline-block h-[9px] rounded bg-primary lg:w-[370px]"></span>
+        </h2>
+        <p className="text-center">
+          تأتي الحملة التوعوية بدعم من مشروع آمن ضمن مشروع تكامل، بتنفيذ مؤسسة
+          عدالة للتنمية القانونية، وبالشراكة مع منظمة سيفرورلد، وبتمويل من
+          الاتحاد الأوروبي.
+        </p>
+      </div>
+      <div className="mt-6 flex flex-wrap justify-center gap-10">
+        {[
+          "/haseen_partners/amen.png",
+          "/haseen_partners/takamol.png",
+          "/haseen_partners/adalah_foundation.png",
+          "/haseen_partners/saferworld.png",
+          "/haseen_partners/en_fundedbytheeu_rgb_pos.png",
+        ].map((logo: string, idx: number) => (
+          <div className="h-18 relative w-44" key={idx}>
+            <Image
+              fill
+              key={idx}
+              src={logo}
+              alt={`شريك ${idx + 1}`}
+              className="object-contain"
+            />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
